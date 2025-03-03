c186db28fc8440cb0b669aba5de33d28fe4b1a5f
derekmeegan <derek.j.meegan@gmail.com>
2025-03-03 16:54:31 -0500

    change stack alias

--- infra/app.py
+++ infra/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 import os
 import aws_cdk as cdk
-from stack import MyServerlessStack
+from stack import PolyMarketStack
 
 app: cdk.App = cdk.App()
-MyServerlessStack(
+PolyMarketStack(
     app, 
     "PolyMarketStack",
     env=cdk.Environment(
--- infra/stack.py
+++ infra/stack.py
@@ -17,7 +17,7 @@ from aws_cdk.aws_s3 import Bucket
 from constructs import Construct
 
 
-class MyServerlessStack(Stack):
+class PolyMarketStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
